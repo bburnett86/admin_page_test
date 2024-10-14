97d96d133af1703ddedb232a4a704ddc9c2a04c6
Bret <breton.burnett@gmail.com>
2024-10-13 23:56:36 -0500

    clean up mobile

--- app/javascript/components/Dashboard/Escalations/Escalations.jsx
+++ app/javascript/components/Dashboard/Escalations/Escalations.jsx
@@ -1,254 +1,64 @@
 import React from "react";
 import '../../App.scss';
-import WebpackerReact from 'webpacker-react'
-
-import {Avatar, Caption, Card, Flex, FlexItem, Icon, Layout, MultipleUsers, Timestamp, Title, SectionSeparator, Body, Detail} from 'playbook-ui'
-
+import WebpackerReact from 'webpacker-react';
+import { Avatar, Card, Flex, FlexItem, Icon, Layout, Body, Detail, Caption } from 'playbook-ui';
 
 const Escalations = (props) => {
-
   const formatType = (str) => {
-    return str
-      .replace(/_/g, ' ') 
-      .replace(/^\w/, (match) => match.toUpperCase()); 
-  }
+    return str.replace(/_/g, ' ').replace(/^\w/, (c) => c.toUpperCase());
+  };
 
-	return (
+  const sections = [
+    { title: 'New', color: 'category_2', key: 'newTickets' },
+    { title: 'Manager Feedback', color: 'category_1', key: 'managerFeedback' },
+    { title: 'Processing', color: 'category_3', key: 'processing' },
+    { title: 'Awaiting Feedback', color: 'category_11', key: 'awaitingFeedback' },
+    { title: 'Approved', color: 'category_12', key: 'approved' },
+  ];
+
+  const renderSection = ({ title, color, key }) => (
+    <Layout.Body layout="content" collapse="xs" position="left">
+      <Card highlight={{ position: 'side', color, paddingY: "none" }} className="card-header first-column">
+        <Caption text={title} />
+      </Card>
+      {props[key].slice(0, 5).map((ticket) => (
+        <Card key={ticket.id} marginTop="none" className="first-column">
+          <Flex align="center" justify="start" orientation="row" className="full-width">
+            <Flex className="full-width" justify="between">
+              <Flex>
+                <FlexItem marginRight="sm">
+                  <Avatar size="sm" name={ticket.assigned_to.first_name + ' ' + ticket.assigned_to.last_name} />
+                </FlexItem>
+                <FlexItem>
+                  <Body text={formatType(ticket.ticket_type)} truncate="1" />
+                </FlexItem>
+              </Flex>
+              <FlexItem>
+                <Icon icon="expand" />
+              </FlexItem>
+            </Flex>
+          </Flex>
+        </Card>
+      ))}
+      {props[key].length > 5 && (
+        <Card key={`${key}ShowMore`} marginTop="none" justifyContent="center" paddingY="sm" className="first-column">
+          <Flex marginTop="none" justify="center" paddingY="none">
+            <Detail text="Show More" truncate="1" bold="true" size="lg" color="link" />
+          </Flex>
+        </Card>
+      )}
+    </Layout.Body>
+  );
+
+  return (
     <div>
-      <Layout layout="kanban"
-        className="escalations-container"
-        display={{
-          xs: "block",
-          sm: "block",
-          md: "block",
-        }}
-      >
-        <Layout.Body
-            layout="content"
-            collapse="xs"
-            position="left"
-        >
-          <Card
-            highlight={{ position: 'side', color: 'category_2', paddingY: "none" }}
-            className="card-header first-column"
-          >
-            <Caption text="New"/>
-          </Card>
-          {props.newTickets.slice(0, 6).map((ticket) => (
-            <Card
-              key={ticket.id}
-              marginTop="none"
-              className="first-column"
-            >
-              <Body 
-              text={formatType(ticket.ticket_type)}
-              truncate="1"
-              />
-            </Card>
-          ))}
-          {props.newTickets.length > 6 && (
-            <Card
-            key={"newTicketsShowMore"}
-            marginTop="none"
-            justifyContent="center"
-            paddingY="sm"
-            className="first-column"
-            >
-              <Flex
-                marginTop="none"
-                justify="center"
-                paddingY="none"
-              >
-
-                <Detail 
-                  text="Show More"
-                  truncate="1"
-                  bold="true"
-                  size="lg"
-                  color="link"
-                />
-              </Flex>
-            </Card>
-          )}
-        </Layout.Body>
-        <Layout.Body>
-        <Card
-          highlight={{ position: 'side', color: 'category_1' }}
-          className="card-header"
-        >
-          <Caption
-            text="Manager Feedback"
-          />
-        </Card>
-        {props.managerFeedback.slice(0, 6).map((ticket) => (
-          <Card
-            key={ticket.id}
-            marginTop="none"
-          >
-            <Body text={formatType(ticket.ticket_type)}
-              truncate="1"
-            />
-          </Card>
-        ))}
-        {props.managerFeedback.length > 6 && (
-            <Card
-            key={"managerFeedbackShowMore"}
-            marginTop="none"
-            justifyContent="center"
-            paddingY="sm"
-            >
-              <Flex
-                marginTop="none"
-                justify="center"
-                paddingY="none"
-              >
-
-                <Detail 
-                  text="Show More"
-                  truncate="1"
-                  bold="true"
-                  size="lg"
-                  color="link"
-                />
-              </Flex>
-            </Card>
-          )}
-        </Layout.Body>
-        <Layout.Body>
-        <Card
-          highlight={{ position: 'side', color: 'category_3' }}
-          className="card-header"
-        >
-          < Caption
-          text ='Processing'
-          />
-        </Card>
-        {props.processing.slice(0, 6).map((ticket) => (
-          <Card
-            key={ticket.id}
-            marginTop="none"
-          >
-            <Body text={formatType(ticket.ticket_type)}
-              truncate="1"
-            />
-          </Card>
-        ))}
-        {props.processing.length > 6 && (
-            <Card
-            key={"processingShowMore"}
-            marginTop="none"
-            justifyContent="center"
-            paddingY="sm"
-            >
-              <Flex
-                marginTop="none"
-                justify="center"
-                paddingY="none"
-              >
-
-                <Detail 
-                  text="Show More"
-                  truncate="1"
-                  bold="true"
-                  size="lg"
-                  color="link"
-                />
-              </Flex>
-            </Card>
-          )}
-        </Layout.Body>
-        <Layout.Body>
-          <Card
-            highlight={{ position: 'side', color: 'category_11' }}
-            className="card-header"
-          >
-            <Caption
-             text='Awaiting Feedback'
-            />
-          </Card>
-          {props.awaitingFeedback.slice(0, 6).map((ticket) => (
-            <Card
-              key={ticket.id}
-              marginTop="none"
-            >
-              <Body text={formatType(ticket.ticket_type)}
-                truncate="1"
-              />
-            </Card>
-          ))}
-          {props.awaitingFeedback.length > 6 && (
-            <Card
-            key={"awaitingFeedbackShowMore"}
-            marginTop="none"
-            justifyContent="center"
-            paddingY="sm"
-            >
-              <Flex
-                marginTop="none"
-                justify="center"
-                paddingY="none"
-              >
-
-                <Detail 
-                  text="Show More"
-                  truncate="1"
-                  bold="true"
-                  size="lg"
-                  color="link"
-                />
-              </Flex>
-            </Card>
-          )}
-        </Layout.Body>
-        <Layout.Body>
-          <Card
-            highlight={{ position: 'side', color: 'category_12' }}
-            className="card-header"
-          >
-            <Caption
-              text='Approved'
-            />
-          </Card>
-          {props.approved.slice(0, 6).map((ticket) => (
-            <Card
-              key={ticket.id}
-              marginTop="none"
-            >
-              <Body text={formatType(ticket.ticket_type)}
-                truncate="1"
-              />
-            </Card>
-          ))}
-          {props.approved.length > 6 && (
-            <Card
-            key={"approvedShowMore"}
-            marginTop="none"
-            justifyContent="center"
-            paddingY="sm"
-            >
-              <Flex
-                marginTop="none"
-                justify="center"
-                paddingY="none"
-              >
-
-                <Detail 
-                  text="Show More"
-                  truncate="1"
-                  bold="true"
-                  size="lg"
-                  color="link"
-                />
-              </Flex>
-            </Card>
-          )}
-        </Layout.Body>
+      <Layout layout="kanban" className="escalations-container" display={{ xs: "block", sm: "block", md: "block" }}>
+        {sections.map(renderSection)}
       </Layout>
     </div>
-  )
-}
-  
-WebpackerReact.setup({Escalations})
+  );
+};
 
+WebpackerReact.setup({ Escalations });
 
-export default Escalations
+export default Escalations;
